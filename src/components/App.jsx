ca5723892ee9ba9ffb78693b35fddfc4f31baaa1
empireofryan <rjohnson2001@gmail.com>
2016-06-22 16:09:03 -0400

    question component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,97 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx'
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [
+        {
+          id: 1,
+          text: 'What is your name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Michael'
+            },
+            {
+              id: 'b',
+              text: 'Brad'
+            },
+            {
+              id: 'c',
+              text: 'Steven'
+            }
+          ],
+            correct: 'b'
+        },
+        {
+          id: 2,
+          text: "What is your mother's name?",
+          choices: [
+            {
+              id: 'a',
+              text: 'Sarah'
+            },
+            {
+              id: 'b',
+              text: 'Sue'
+            },
+            {
+              id: 'c',
+              text: 'Donna'
+            }
+          ],
+            correct: 'c'
+        },
+        {
+          id: 1,
+          text: "What is your father's name?",
+          choices: [
+            {
+              id: 'a',
+              text: 'Bobby'
+            },
+            {
+              id: 'b',
+              text: 'Harry'
+            },
+            {
+              id: 'c',
+              text: 'Wayne'
+            }
+          ],
+            correct: 'c'
+        },
+        {
+          id: 1,
+          text: "What is your friend's name?",
+          choices: [
+            {
+              id: 'a',
+              text: 'John'
+            },
+            {
+              id: 'b',
+              text: 'Paul'
+            },
+            {
+              id: 'c',
+              text: 'Chris'
+            }
+          ],
+            correct: 'a'
+        }
+      ],
+      score : 0,
+      current: 1
+    }
+  }
   render(){
     return(
       <div>
-        APP
+        <QuestionList {...this.state} />
       </div>
     )
   }
